d05f9d58dcb7c0866066a756dae1df5d22cefa98
Ramilatif <latiframi18@gmail.com>
2026-01-25 12:51:15 +0100

    asm03 correct

---
+++ asm03/asm03.s
@@ -0,0 +1,46 @@
+global _start
+default rel
+section .rodata
+msg:    db "1337", 10
+len:    equ $ - msg
+
+section .text
+_start:
+
+        mov     rbx, rsp
+        
+        mov     rax, [rbx]
+        cmp     rax, 2
+        jne     .exit
+        jl      .exit1
+
+        
+        mov     rsi, [rbx + 16]
+        
+
+        cmp     byte [rsi + 0], '4'
+        jne     .exit1
+        cmp     byte [rsi + 1], '2'
+        jne     .exit1
+        cmp     byte [rsi + 2], 0
+        jne     .exit1
+        jmp     .win
+
+
+.exit:
+    mov     rax, 60
+    mov     rdi, 0             ; SYS_exit
+    syscall
+
+.exit1:
+    mov     rax, 60
+    mov     rdi, 1             ; SYS_exit
+    syscall
+
+.win:
+    mov     rax, 1
+    mov     rdi, 1
+    lea     rsi, [rel msg]
+    mov     rdx, len
+    syscall
+    jmp     .exit
